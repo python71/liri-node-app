a6390d03ca1b372ada5753218b852d42a33b9f82
John Brown <john.brown@libertymutual.com>
2019-02-23 20:49:01 -0500

    fixed errors

--- liri.js
+++ liri.js
@@ -14,54 +14,64 @@ var request = process.argv[2];
 var requestItem = process.argv.slice(3).join(" ");
 
 function getConcert() {
-  // Then run a request with axios to the bandintown API with the movie specified
-  axios.get("https://rest.bandsintown.com/artists/" + requestItem +"/events?app_id=codingbootcamp").then(
-    function(response) {
-      console.log("Band Name: " + requestItem.toUpperCase());
-      console.log("Venue: " + response.data[0].venue.name);
-      console.log("City: " + response.data[0].venue.city);
-      console.log("State: " + response.data[0].venue.region);
-      var time = response.data[0].datetime
-      var newTime = moment(time).format("MM/DD/YYYY")
-      console.log("Date: " + newTime + "\n");
+  if (!requestItem) {
+    console.log("Please enter a band name");
+    process.exit();
+  } 
+  else {
+    // Then run a request with axios to the bandintown API with the movie specified
+    axios.get("https://rest.bandsintown.com/artists/" + requestItem +"/events?app_id=codingbootcamp").then(
+      function(response) {
+        console.log("Band Name: " + requestItem.toUpperCase());
+        console.log("Venue: " + response.data[0].venue.name);
+        console.log("City: " + response.data[0].venue.city);
+        console.log("State: " + response.data[0].venue.region);
+        var time = response.data[0].datetime
+        var newTime = moment(time).format("MM/DD/YYYY")
+        console.log("Date: " + newTime + "\n");
 
-      //write to log.txt file
-      fs.appendFile("log.txt", "Band Name: " + requestItem.toUpperCase() + 
-            "\nVenue: " + response.data[0].venue.name +
-            "\nCity: " + response.data[0].venue.city +
-            "\nState: " + response.data[0].venue.region +
-            "Date: " + newTime + "\n\n", function(err) {
-        if (err) {
-          return console.log(err);
-        }
-      })
-    }
-  );
+        //write to log.txt file
+        fs.appendFile("log.txt", "Band Name: " + requestItem.toUpperCase() + 
+                      "\nVenue: " + response.data[0].venue.name +
+                      "\nCity: " + response.data[0].venue.city +
+                      "\nState: " + response.data[0].venue.region +
+                      "Date: " + newTime + "\n\n", function(err) {
+          if (err) {
+            return console.log(err);
+          }
+        })
+      }
+    );
+  }
 }
 
 function getSpotify() {
+  if (!requestItem) {
+    console.log("Please enter a song name")
+  }
+  else {
+    spotify
+      .search({ type: 'track', query: requestItem, limit: 20 })
+      .then(function(response) {
+          console.log("Band Name: " + response.tracks.items[0].artists[0].name);
+          console.log("Album Name: " + response.tracks.items[0].album.name);
+          console.log("Song Name: " + requestItem);
+          console.log("Song Link: " + response.tracks.items[0].artists[0].external_urls.spotify + "\n")
 
-  spotify
-    .search({ type: 'track', query: requestItem, limit: 20 })
-    .then(function(response) {
-        console.log("Band Name: " + response.tracks.items[0].artists[0].name);
-        console.log("Album Name: " + response.tracks.items[0].album.name);
-        console.log("Song Name: " + requestItem);
-        console.log("Song Link: " + response.tracks.items[0].artists[0].external_urls.spotify + "\n")
-
-              //write to log.txt file
-        fs.appendFile("log.txt", "Band Name: " + response.tracks.items[0].artists[0].name + 
-        "\nAlbum Name: " + response.tracks.items[0].album.name +
-        "\nSong Name: " + requestItem +
-        "\nSong Link: " + response.tracks.items[0].artists[0].external_urls.spotify + "\n\n", function(err) {
-        if (err) {
-          return console.log(err);
-          }
+          //write to log.txt file
+          fs.appendFile("log.txt", "Band Name: " + response.tracks.items[0].artists[0].name + 
+                        "\nAlbum Name: " + response.tracks.items[0].album.name +
+                        "\nSong Name: " + requestItem +
+                        "\nSong Link: " + response.tracks.items[0].artists[0].external_urls.spotify + "\n\n", function(err) {
+          if (err) {
+            return console.log(err);
+            }
+          })
         })
-      })
-    .catch(function(err) {
-        console.log(err);
-    });
+      .catch(function(err) {
+          console.log(err);
+      });
+    }
   }
 
 
@@ -103,13 +113,10 @@ function getRandomStuff() {
         var newData = data.split(",");
         console.log(newData);
 
-        // determine parameters
-        // function to use
         request = newData[0];
         // what to look up
         requestItem = newData[1];
-        // getSpotify();
-        
+
         if (request === "concert-this") {
           getConcert();
         }
